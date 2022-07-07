7930bae9fbc0a8d9ef89fcb2180f9e2f39718745
DevSam7t3 <samirashid54222@gmail.com>
2022-07-07 10:20:26 +0500

    Just completed the project

---
+++ app.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const exphbs = require('express-handlebars');
+
+require('dotenv').config();
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+// Parsing middleware
+// Parse application/x-www-form-urlencoded
+app.use(express.urlencoded({ extended: true })); // New
+
+// Parse application/json
+app.use(express.json()); // New
+
+// Static Files
+app.use(express.static('public'));
+
+// Templating Engine
+const handlebars = exphbs.create({ extname: '.hbs', });
+app.engine('.hbs', handlebars.engine);
+app.set('view engine', '.hbs');
+
+const routes = require('./server/routes/user');
+app.use('/', routes);
+
+app.listen(port, () => console.log(`Listening on port ${port}`));
